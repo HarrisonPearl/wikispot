7aa2069a002671dc3593e02eb2c36c790b9ad3b9
HarrisonPearl <Hspearl@gmail.com>
2021-05-05 17:30:22 -0400

    generates cards for nearby wikis

--- wikispot.js
+++ wikispot.js
@@ -4,6 +4,54 @@ let compass;
 let isIOS;
 
 function init() {
+  if (window.navigator.geolocation) {
+    // Geolocation available
+    window.navigator.geolocation
+      .getCurrentPosition(generateCards, console.log);          
+  }   
+
+  function generateCards(userPos){
+    var url = "https://en.wikipedia.org/w/api.php"; 
+
+    var params = {
+      action: "query",
+      list: "geosearch",
+      gscoord: userPos.coords.latitude.toString() + "|" + userPos.coords.longitude.toString(),
+      gsradius: "800",
+      gslimit: "20",
+      format: "json"
+    };
+
+    var nearbyWikis = [];
+
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+    fetch(url)
+      .then(function(response){return response.json();})
+      .then(function(response) {
+        var pages = response.query.geosearch;
+        for (var place in pages) {
+          console.log(pages[place].title);
+          console.log(pages[place].lat);
+          console.log(pages[place].lon);
+          nearbyWikis.push(pages[place]);
+        }
+        for (i = 0; i < nearbyWikis.length; i++){
+          console.log(nearbyWikis[i]);
+          var distanceFromUser = coordDistance(nearbyWikis[i].lat, nearbyWikis[i].lon, userPos.coords.latitude, userPos.coords.longitude, "M");
+          var element = document.createElement('div');
+          element.setAttribute("class", "wikicard")
+          element.innerHTML = nearbyWikis[i].title;
+          document.getElementById("card-list").appendChild(element);
+        }
+        console.log("num of wikis");
+        console.log(nearbyWikis.length);
+      })
+      .catch(function(error){console.log(error);});
+  }
+
+  
   compassCircle = document.querySelector(".compass-circle");
   startBtn = document.querySelector(".start-btn");
   myPoint = document.querySelector(".my-point");
@@ -13,6 +61,28 @@ function init() {
   );
 
   startCompass();
+}
+
+function coordDistance(lat1, lon1, lat2, lon2, unit) {
+	if ((lat1 == lat2) && (lon1 == lon2)) {
+		return 0;
+	}
+	else {
+		var radlat1 = Math.PI * lat1/180;
+		var radlat2 = Math.PI * lat2/180;
+		var theta = lon1-lon2;
+		var radtheta = Math.PI * theta/180;
+		var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+		if (dist > 1) {
+			dist = 1;
+		}
+		dist = Math.acos(dist);
+		dist = dist * 180/Math.PI;
+		dist = dist * 60 * 1.1515;
+		if (unit=="K") { dist = dist * 1.609344 }
+		if (unit=="N") { dist = dist * 0.8684 }
+		return dist;
+	}
 }
 
 function startCompass() {
