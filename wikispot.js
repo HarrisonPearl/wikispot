a747fd9b46fb68f6bf715b27ed07772e965c55c1
HarrisonPearl <Hspearl@gmail.com>
2021-05-06 22:48:46 -0400

    indented card text

--- wikispot.js
+++ wikispot.js
@@ -74,7 +74,7 @@ function generateCards(userPos){
         let element = document.createElement('div');
         element.setAttribute("class", "wikicard")
         element.innerHTML = 
-          "<a href=" + makeWikiLink(nearbyWikis[i].title) + ">" + nearbyWikis[i].title + "</a>" +
+          "<a class='cardText' href=" + makeWikiLink(nearbyWikis[i].title) + ">" + nearbyWikis[i].title + "</a>" +
           "<a class='compass' href='https://www.google.com/maps/search/?api=1&query=" + nearbyWikis[i].lat.toString() + "," + nearbyWikis[i].lon.toString() + "'>" +
             "<div class='distance'>" + distanceFromUser.toFixed(2).toString() + " mi</div>" +
             "<div class='arrow'></div>" +
