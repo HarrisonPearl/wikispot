25b0308804da9563f105a4ba5b56b0a601af3655
HarrisonPearl <Hspearl@gmail.com>
2021-06-03 15:59:51 -0400

    adds auth and database hook up

--- public/wikispot.js
+++ public/wikispot.js
@@ -1,24 +1,91 @@
 let compassCircles;
-let myPoint;
 let compass;
 let isIOS;
+let isMobile = null;
+
+firebase.auth()
+  .getRedirectResult()
+  .then((result) => {
+    if (result.credential) {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // ...
+    }
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user.uid);
+    
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+    console.log("No User Signed In");
+  });
+
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log("got one", user);
+    loginBtn = document.querySelector(".login-btn");
+    loginBtn.remove();
+    let mySpotButton = document.createElement('button');
+    mySpotButton.setAttribute("class", "button my-spots-btn");
+    mySpotButton.setAttribute("id", "mSpotButton");
+    mySpotButton.innerHTML = "hi, " + user.displayName.substr(0,user.displayName.indexOf(' '));;
+    document.getElementById("body").appendChild(mySpotButton);
+
+    const dbRefObject = firebase.database().ref()
+      .child('users')
+      .child('VYES4o37U4TJnTAkrdr7FfLkzA42')
+      .child('wikispots')
+      .child('0');
+    
+    dbRefObject.on('value', handleSnap(snap));
+
+  }
+  else {
+    console.log("no user, didnt get one");
+  }
+});
+
+function handleSnap(snap) {
+  console.log(snap.val())
+}
 
 function init() {
+  if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))){ 
+    isMobile = true;
+  }
+  if (isMobile){
+    window.addEventListener( "pageshow", function ( event ) {
+      var historyTraversal = event.persisted || (
+        typeof window.performance.getEntriesByType("navigation")[0] != "undefined" && 
+        window.performance.getEntriesByType("navigation")[0].type === "back_forward" 
+      );
+      if (historyTraversal) {
+        window.scrollTo(0, 1);
+      }
+    });
+  }
+
   startBtn = document.querySelector(".start-btn");
-  myPoint = document.querySelector(".my-point");
+  startBtn.addEventListener("click", buildCardList);
+
   loginBtn = document.querySelector(".login-btn");
+  loginBtn.addEventListener("click", openAuthWindow);
+
   isIOS = (
     navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
     navigator.userAgent.match(/AppleWebKit/)
-  );
-  loginBtn.addEventListener("click", openAuthWindow);
-  // check if Geolocation available 
-  if (window.navigator.geolocation) {
-    startBtn.addEventListener("click", buildCardList);          
-  }
-  else {
-    alert("Please enable geolocation services")
-  }
+  ); 
 }
 
 function openAuthWindow() {
@@ -29,7 +96,7 @@ function openAuthWindow() {
 
   let closeWindow = document.createElement('div');
   closeWindow.setAttribute("class", "button close-window");
-  //closeWindow.innerHTML = "X";
+  closeWindow.innerHTML = "X";
   closeWindow.addEventListener("click", closeAuthWindow);
   document.getElementById("auth-win").appendChild(closeWindow);
 
@@ -91,30 +158,6 @@ function openAuthWindow() {
 function signInWithRedirect(){
   let provider = new firebase.auth.GoogleAuthProvider();
   firebase.auth().signInWithRedirect(provider);
-  firebase.auth()
-  .getRedirectResult()
-  .then((result) => {
-    console.log(result);
-    if (result.credential) {
-      /** @type {firebase.auth.OAuthCredential} */
-      var credential = result.credential;
-
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      var token = credential.accessToken;
-      // ...
-    }
-    // The signed-in user info.
-    var user = result.user;
-  }).catch((error) => {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
 }
 
 function closeAuthWindow(){
@@ -122,24 +165,29 @@ function closeAuthWindow(){
 }
 
 function buildCardList(){
-  window.navigator.geolocation
-    .getCurrentPosition(generateCards, console.log);
+    if (isIOS) {
+      DeviceOrientationEvent.requestPermission()
+        .then((response) => {
+          if (response === "granted") {
+            window.addEventListener("deviceorientation", handler, true);
+          } else {
+            alert("has to be allowed!");
+          }
+        })
+        .catch((response) => alert(response));
+    } 
+    else {
+      window.addEventListener("deviceorientationabsolute", handler, true);
+    }
 
-  if (isIOS) {
-    DeviceOrientationEvent.requestPermission()
-      .then((response) => {
-        if (response === "granted") {
-          window.addEventListener("deviceorientation", handler, true);
-        } else {
-          alert("has to be allowed!");
-        }
-      })
-      .catch((response) => alert(response));
-  } else {
-    window.addEventListener("deviceorientationabsolute", handler, true);
+    if (window.navigator.geolocation) {
+      window.navigator.geolocation
+        .getCurrentPosition(generateCards, console.log);
+      startBtn.remove();        
+    }
+  else {
+    alert("Please enable geolocation services")
   }
-
-  startBtn.remove();
 }
 
 function generateCards(userPos){
@@ -177,14 +225,14 @@ function generateCards(userPos){
         let angleFromUser = calcDegreeToPoint(userPos.coords.latitude, userPos.coords.longitude, nearbyWikis[i].lat, nearbyWikis[i].lon,)
         let element = document.createElement('div');
         element.setAttribute("class", "wikicard")
-        /*element.innerHTML = 
+        element.innerHTML = 
           "<a class='cardText' href=" + makeWikiLink(nearbyWikis[i].title) + ">" + nearbyWikis[i].title + "</a>" +
           "<a class='compass' href='https://www.google.com/maps/search/?api=1&query=" + nearbyWikis[i].lat.toString() + "," + nearbyWikis[i].lon.toString() + "'>" +
             "<div class='distance'>" + distanceFromUser.toFixed(2).toString() + " mi</div>" +
             "<div class='arrow'></div>" +
             (distanceFromUser > 0.2 ? "<div class='compass-circle' dist=" + distanceFromUser.toString() + " ang=" + angleFromUser.toString() + "></div>" : "") +
             "<div class='my-point'></div>" +
-          "</div>";*/
+          "</div>";
         document.getElementById("card-list").appendChild(element);
       }
       compassCircles = document.querySelectorAll(".compass-circle");
