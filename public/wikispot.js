7be6e883146744f0b23c357228e0a1bba6ad01a5
HarrisonPearl <Hspearl@gmail.com>
2021-06-08 11:46:32 -0400

    user and nearby card list generations

--- public/wikispot.js
+++ public/wikispot.js
@@ -2,6 +2,13 @@ let compassCircles;
 let compass;
 let isIOS;
 let isMobile = null;
+let usersWikis = [];
+let showUserWikis = false;
+
+/////////////
+// Check for signed in user, load their wikis in userWikis if a user exists
+////////////
+
 
 firebase.auth()
   .getRedirectResult()
@@ -41,13 +48,12 @@ firebase.auth().onAuthStateChanged(function(user) {
     mySpotButton.innerHTML = "hi, " + user.displayName.substr(0,user.displayName.indexOf(' '));;
     document.getElementById("body").appendChild(mySpotButton);
 
-    const dbRefObject = firebase.database().ref()
+    const dbRefUserSpots = firebase.database().ref()
       .child('users')
-      .child('VYES4o37U4TJnTAkrdr7FfLkzA42')
+      .child(user.uid)
       .child('wikispots')
-      .child('0');
     
-    dbRefObject.on('value', handleSnap(snap));
+      dbRefUserSpots.on('value', snap => handleSnap(snap));
 
   }
   else {
@@ -56,10 +62,20 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 
 function handleSnap(snap) {
-  console.log(snap.val())
+  for (i = 0; i < Object.keys(snap.val()).length; i++){
+    console.log(snap.val()[i]);
+    usersWikis.push(snap.val()[i]);
+  }
+  
 }
 
+///////
+// init
+///////
+
+
 function init() {
+  // return events bring user to last scroll position
   if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
     || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))){ 
     isMobile = true;
@@ -76,11 +92,15 @@ function init() {
     });
   }
 
+  // Button setup
   startBtn = document.querySelector(".start-btn");
   startBtn.addEventListener("click", buildCardList);
 
   loginBtn = document.querySelector(".login-btn");
   loginBtn.addEventListener("click", openAuthWindow);
+
+  userWikisBtn = document.querySelector(".user-wikis-btn");
+  userWikisBtn.addEventListener("click", switchCardList);
 
   isIOS = (
     navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
@@ -88,6 +108,14 @@ function init() {
   ); 
 }
 
+// card list control
+
+function switchCardList(){
+  showUserWikis = !showUserWikis;
+  buildCardList()
+}
+
+// Auth window
 function openAuthWindow() {
   let authWindow = document.createElement('div');
   authWindow.setAttribute("class", "auth-window");
@@ -164,27 +192,31 @@ function closeAuthWindow(){
   document.getElementById("auth-win").remove();
 }
 
+// card list building
 function buildCardList(){
-    if (isIOS) {
-      DeviceOrientationEvent.requestPermission()
-        .then((response) => {
-          if (response === "granted") {
-            window.addEventListener("deviceorientation", handler, true);
-          } else {
-            alert("has to be allowed!");
-          }
-        })
-        .catch((response) => alert(response));
-    } 
-    else {
-      window.addEventListener("deviceorientationabsolute", handler, true);
-    }
+  clearCardList();
+  // get orientation permission
+  if (isIOS) {
+    DeviceOrientationEvent.requestPermission()
+      .then((response) => {
+        if (response === "granted") {
+          window.addEventListener("deviceorientation", handler, true);
+        } else {
+          alert("has to be allowed!");
+        }
+      })
+      .catch((response) => alert(response));
+  } 
+  else {
+    window.addEventListener("deviceorientationabsolute", handler, true);
+  }
 
-    if (window.navigator.geolocation) {
-      window.navigator.geolocation
-        .getCurrentPosition(generateCards, console.log);
-      startBtn.remove();        
-    }
+  // ask for user position permission, if granted, generate cards
+  if (window.navigator.geolocation) {
+    window.navigator.geolocation
+      .getCurrentPosition(generateCards, console.log);
+    startBtn.remove();        
+  }
   else {
     alert("Please enable geolocation services")
   }
@@ -193,7 +225,7 @@ function buildCardList(){
 function generateCards(userPos){
   var url = "https://en.wikipedia.org/w/api.php"; 
 
-  // set gslimit
+  // set gslimits
   var params = {
     action: "query",
     list: "geosearch",
@@ -214,27 +246,22 @@ function generateCards(userPos){
       let pages = response.query.geosearch;
       console.log(pages[0]);
       for (let place in pages) {
-        //console.log(pages[place].title);
-        //console.log(pages[place]);
-        //console.log(pages[place].lat);
-        //console.log(pages[place].lon);
         nearbyWikis.push(pages[place]);
       }
-      for (i = 0; i < nearbyWikis.length; i++){
-        let distanceFromUser = coordDistance(nearbyWikis[i].lat, nearbyWikis[i].lon, userPos.coords.latitude, userPos.coords.longitude, "M");
-        let angleFromUser = calcDegreeToPoint(userPos.coords.latitude, userPos.coords.longitude, nearbyWikis[i].lat, nearbyWikis[i].lon,)
-        let element = document.createElement('div');
-        element.setAttribute("class", "wikicard")
-        element.innerHTML = 
-          "<a class='cardText' href=" + makeWikiLink(nearbyWikis[i].title) + ">" + nearbyWikis[i].title + "</a>" +
-          "<a class='compass' href='https://www.google.com/maps/search/?api=1&query=" + nearbyWikis[i].lat.toString() + "," + nearbyWikis[i].lon.toString() + "'>" +
-            "<div class='distance'>" + distanceFromUser.toFixed(2).toString() + " mi</div>" +
-            "<div class='arrow'></div>" +
-            (distanceFromUser > 0.2 ? "<div class='compass-circle' dist=" + distanceFromUser.toString() + " ang=" + angleFromUser.toString() + "></div>" : "") +
-            "<div class='my-point'></div>" +
-          "</div>";
-        document.getElementById("card-list").appendChild(element);
+      //cards are generated from nearby wikis or user wikis depending
+      if (showUserWikis){
+        for (i = 0; i < usersWikis.length; i++){
+          createCard(usersWikis[i].title, usersWikis[i].lat, usersWikis[i].lon, userPos);
+        }
       }
+      else {
+        for (i = 0; i < nearbyWikis.length; i++){
+          createCard(nearbyWikis[i].title, nearbyWikis[i].lat, nearbyWikis[i].lon, userPos);
+        }
+      }
+      
+
+
       compassCircles = document.querySelectorAll(".compass-circle");
       console.log("num of wikis");
       console.log(nearbyWikis.length);
@@ -242,6 +269,21 @@ function generateCards(userPos){
     .catch(function(error){console.log(error);});
 }
 
+function createCard(title, lat, lon, userPos){
+  let distanceFromUser = coordDistance(lat, lon, userPos.coords.latitude, userPos.coords.longitude, "M");
+  let angleFromUser = calcDegreeToPoint(userPos.coords.latitude, userPos.coords.longitude, lat, lon,)
+  let element = document.createElement('div');
+  element.setAttribute("class", "wikicard")
+  element.innerHTML = 
+    "<a class='cardText' href=" + makeWikiLink(title) + ">" + title + "</a>" +
+    "<a class='compass' href='https://www.google.com/maps/search/?api=1&query=" + lat.toString() + "," + lon.toString() + "'>" +
+      "<div class='distance'>" + distanceFromUser.toFixed(2).toString() + " mi</div>" +
+      "<div class='arrow'></div>" +
+      (distanceFromUser > 0.2 ? "<div class='compass-circle' dist=" + distanceFromUser.toString() + " ang=" + angleFromUser.toString() + "></div>" : "") +
+      "<div class='my-point'></div>" +
+    "</div>";
+  document.getElementById("card-list").appendChild(element);
+}
 function makeWikiLink(title) {
   let link = "https://en.wikipedia.org/wiki/" + title.replaceAll(" ", "_");
   return link;
@@ -293,6 +335,10 @@ function calcDegreeToPoint(lat1, lon1, lat2, lon2) {
         Math.sin(phi) * Math.cos(lambdaK - lambda)
     );
   return Math.round(psi);
+}
+
+function clearCardList(){
+  wikiCards = document.querySelectorAll(".wikicard").forEach(wc => wc.remove());;
 }
 
 window.onload = init;
